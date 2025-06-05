3e25e562aa4832a72113f6bd36155f96b5021d98
Brittany Halterman <halterma@gmail.com>
2025-06-05 15:06:31 -0400

    Refactor card deck creation into RSpec let

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -1,22 +1,17 @@
 require_relative '../lib/deck.rb'
 
 describe 'Deck' do
+  let(:deck) { Deck.new }
   describe 'initialize' do
     it 'should initialize with a full deck of cards' do
-      deck = Deck.new
-
       expect(deck.cards.count).to eq Deck::FULL_DECK_SIZE
     end
 
     it 'initialize with 52 unique cards' do
-      deck = Deck.new
-
       expect(deck.cards.uniq(&:to_s).count).to eq Deck::FULL_DECK_SIZE
     end
 
     it 'should include every rank' do
-      deck = Deck.new
-      
       Card::RANK.each do |rank|
         expect(deck.cards.map(&:rank)).to include(rank)
       end
