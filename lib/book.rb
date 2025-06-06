d3ce8b69a9f0382bbbb3dbdaf5c3caab5fd9a7c7
Brittany Halterman <halterma@gmail.com>
2025-06-06 08:43:08 -0400

    Initialize a book with four cards

--- lib/book.rb
+++ lib/book.rb
@@ -1,3 +1,7 @@
 class Book
-  
+  attr_accessor :book_cards
+
+  def initialize(card1, card2, card3, card4)
+    @book_cards = [card1, card2, card3, card4]
+  end
 end
--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -1,5 +1,17 @@
 require_relative '../lib/book.rb'
+require_relative '../lib/card.rb'
 
 describe 'Book' do
-  
+  it 'should initialize with four cards' do
+    card1 = Card.new('A', 'C')
+    card2 = Card.new('A', 'D')
+    card3 = Card.new('A', 'H')
+    card4 = Card.new('A', 'S')
+    book = Book.new(card1, card2, card3, card4)
+
+    expect(book.book_cards.count).to eq 4
+  end
+  it 'should contain cards of only one rank'
+
+  it 'should contains a card of each suit'
 end
