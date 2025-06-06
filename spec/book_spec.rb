514196d9f52a8c466b1a12504fb36a95dd9d3280
Brittany Halterman <halterma@gmail.com>
2025-06-06 09:49:30 -0400

    Rafactor book spec

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -2,19 +2,17 @@ require_relative '../lib/book.rb'
 require_relative '../lib/card.rb'
 
 describe 'Book' do
-  it 'should initialize with four cards' do
-    card1 = Card.new('A', 'C')
-    card2 = Card.new('A', 'D')
-    card3 = Card.new('A', 'H')
-    card4 = Card.new('A', 'S')
-    book = Book.new(card1, card2, card3, card4)
+  let(:card1) { Card.new('A', 'C') }
+  let(:card2) { Card.new('A', 'D') }
+  let(:card3) { Card.new('A', 'H') }
+  let(:card4) { Card.new('A', 'S') }
 
+  let(:book) { Book.new(card1, card2, card3, card4) }
+
+  it 'should initialize with four cards' do
     expect(book.book_cards.count).to eq 4
   end
   it 'should contain cards of only one rank' do
-    card1 = Card.new('A', 'C')
-    card2 = Card.new('A', 'D')
-    card3 = Card.new('A', 'H')
     card4 = Card.new('K', 'S')
 
     expect{
@@ -23,12 +21,6 @@ describe 'Book' do
   end
 
   it 'should contain a card of each suit' do
-    card1 = Card.new('A', 'C')
-    card2 = Card.new('A', 'D')
-    card3 = Card.new('A', 'H')
-    card4 = Card.new('A', 'S')
-    book = Book.new(card1, card2, card3, card4)
-
     Card::SUIT.each do |suit|
       expect(book.book_cards.map(&:suit)).to include(suit)
     end
