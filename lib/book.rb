c24ce29e66ac9d91f1b44f412cd5fff98c463919
Brittany Halterman <halterma@gmail.com>
2025-06-06 09:24:19 -0400

    Confirm that a book has cards of all one rank and every suit

--- lib/book.rb
+++ lib/book.rb
@@ -3,5 +3,12 @@ class Book
 
   def initialize(card1, card2, card3, card4)
     @book_cards = [card1, card2, card3, card4]
+
+    book_cards.map do |card|
+      unless card.rank == book_cards[0].rank
+        raise StandardError, "cards do not all have the same rank"
+      end
+    end
   end
 end
+
--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -11,7 +11,26 @@ describe 'Book' do
 
     expect(book.book_cards.count).to eq 4
   end
-  it 'should contain cards of only one rank'
+  it 'should contain cards of only one rank' do
+    card1 = Card.new('A', 'C')
+    card2 = Card.new('A', 'D')
+    card3 = Card.new('A', 'H')
+    card4 = Card.new('K', 'S')
 
-  it 'should contains a card of each suit'
+    expect{
+      Book.new(card1, card2, card3, card4)
+    }.to raise_error StandardError
+  end
+
+  it 'should contain a card of each suit' do
+    card1 = Card.new('A', 'C')
+    card2 = Card.new('A', 'D')
+    card3 = Card.new('A', 'H')
+    card4 = Card.new('A', 'S')
+    book = Book.new(card1, card2, card3, card4)
+
+    Card::SUIT.each do |suit|
+      expect(book.map(&:suit)).to include(suit)
+    end
+  end
 end
