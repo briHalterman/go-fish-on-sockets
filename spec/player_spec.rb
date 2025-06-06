11645e31320fd6e5c2976675ac50e71c24a59735
Brittany Halterman <halterma@gmail.com>
2025-06-06 13:39:21 -0400

    Refactor player spec to create player in let

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -2,29 +2,24 @@ require_relative '../lib/player.rb'
 require_relative '../lib/card.rb'
 
 describe 'Player' do
+  let(:player) { Player.new('Player') }
+
   describe '#initialize' do
     it 'should initialize with a name' do
-      player = Player.new('Player')
-
       expect(player.name).to eq 'Player'
     end
 
     it 'should initialize with an empty hand' do
-      player = Player.new('Player')
-
       expect(player.hand).to be_empty
     end
 
     it 'should initialize with an empty book stack' do
-      player = Player.new('Player')
-
       expect(player.book_stack.count).to eq 0
     end
   end
 
   describe '#accept_card' do
     it 'should add a card to a player\'s hand' do
-      player = Player.new('Player')
       card = Card.new('A', 'H')
       player.accept_card(card)
 
@@ -37,7 +32,6 @@ describe 'Player' do
   end
 
   describe '#handle_book' do
-    it 'should check a player\'s hand for a set of cards with matching ranks'
 
     it 'should make a book'
 
